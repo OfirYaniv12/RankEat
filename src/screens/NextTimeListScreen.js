63794bd076aec780321d805d79141cd063426620
OfirYaniv12 <ofir1519@gmail.com>
2026-05-27 15:36:38 +0300

    fix(wishlist): increase header paddingTop to clear global navigation buttons

--- src/screens/NextTimeListScreen.js
+++ src/screens/NextTimeListScreen.js
@@ -255,7 +255,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
     paddingHorizontal: SPACING.lg,
-    paddingTop: SPACING.xl + 12,
+    paddingTop: SPACING.xl + 60,
     paddingBottom: SPACING.md,
     width: '100%',
     zIndex: 10,
